e8daf6f0e199645641c471b9ed079f7c78a2c3fb
aymenzoauoui <aymen.zouaoui@esprit.tn>
2024-11-01 23:34:38 +0100

    Update FoyerAspect.java

--- src/main/java/tn/esprit/spring/aop/FoyerAspect.java
+++ src/main/java/tn/esprit/spring/aop/FoyerAspect.java
@@ -1,4 +1,4 @@
-package tn.esprit.spring.AOP;
+package tn.esprit.spring.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -15,22 +15,22 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class FoyerAspect {
     //Méthode: Advice
-    @Before("execution(* tn.esprit.spring.Services..*.*(..))")
+    @Before("execution(* tn.esprit.spring.services..*.*(..))")
     public void method(JoinPoint jp){
         log.info("hello "+jp.getSignature().getName());
     }
-    @After("execution(* tn.esprit.spring.Services..*.*(..))")
+    @After("execution(* tn.esprit.spring.services..*.*(..))")
     public void methoAfter(JoinPoint jp){
         log.info("Bye bye "+jp.getSignature().getName());
     }
 
-    @Around("execution(* tn.esprit.spring.Services..*.*(..))")
+    @Around("execution(* tn.esprit.spring.services..*.*(..))")
     public Object profile(ProceedingJoinPoint pjp) throws Throwable
     {
         long start = System.currentTimeMillis();
         Object obj = pjp.proceed();
         long elapsedTime = System.currentTimeMillis() - start;
         log.info("Method execution time: " + elapsedTime + " milliseconds.");
-        return null;
+        return obj;
     }
 }
