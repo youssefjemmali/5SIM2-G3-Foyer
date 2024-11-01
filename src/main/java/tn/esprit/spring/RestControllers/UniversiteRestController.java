d1d708827c041bf93bded8c8e44b5057cf7e2812
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-11-01 16:27:54 +0000

    making UniversiteRestController methods public

--- src/main/java/tn/esprit/spring/RestControllers/UniversiteRestController.java
+++ src/main/java/tn/esprit/spring/RestControllers/UniversiteRestController.java
@@ -15,27 +15,27 @@ public class UniversiteRestController {
     IUniversiteService service;
 
     @PostMapping("addOrUpdate")
-    Universite addOrUpdate(@RequestBody Universite u) {
+    public Universite addOrUpdate(@RequestBody Universite u) {
         return service.addOrUpdate(u);
     }
 
     @GetMapping("findAll")
-    List<Universite> findAll() {
+    public List<Universite> findAll() {
         return service.findAll();
     }
 
     @GetMapping("findById")
-    Universite findById(@RequestParam long id) {
+    public Universite findById(@RequestParam long id) {
         return service.findById(id);
     }
 
     @DeleteMapping("delete")
-    void delete(@RequestBody Universite u) {
+    public void delete(@RequestBody Universite u) {
         service.delete(u);
     }
 
     @DeleteMapping("deleteById")
-    void deleteById(@RequestParam long id) {
+    public void deleteById(@RequestParam long id) {
         service.deleteById(id);
     }
 
